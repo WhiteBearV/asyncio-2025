c0ed6784caa00226609c06b6b64871864cccdca4
WhiteBearV <kusunzaza096@gmail.com>
2025-06-25 11:47:09 +0700

    push9

--- assignment04/asyncio09.py
+++ assignment04/asyncio09.py
@@ -1,1 +1,27 @@
 # cancel task
+
+import asyncio
+
+async def slow_task():
+
+    await asyncio.sleep(3)
+
+async def main():
+
+    task = asyncio.create_task(slow_task())
+
+    print("Cancel task in 1 sec")
+
+    await asyncio.sleep(1)
+
+    task.cancel()
+
+    try:
+
+        await task
+
+    except asyncio.CancelledError:
+
+        print("Task has been canceled")
+
+asyncio.run(main())
