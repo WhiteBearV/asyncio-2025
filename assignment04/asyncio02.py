065165bb40a5a81a803ddbcad4da1aa035df7faf
WhiteBearV <kusunzaza096@gmail.com>
2025-06-25 10:45:07 +0700

    push2

--- assignment04/asyncio02.py
+++ assignment04/asyncio02.py
@@ -1,1 +1,19 @@
 # Create 1 Task with High-Level API
+
+import asyncio
+
+async def do_something():
+
+    print("Start working...")
+
+    await asyncio.sleep(2)
+
+    print("Work is done!")
+
+async def main():
+
+    task = asyncio.create_task(do_something())
+
+    await task # Waiting for task to complete
+
+asyncio.run(main())
