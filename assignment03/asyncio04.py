0e235eb1f5735d3396bb9a6a8e2f201257ac2e97
WhiteBearV <kusunzaza096@gmail.com>
2025-06-25 10:18:51 +0700

    รอบ4

--- assignment03/asyncio04.py
+++ assignment03/asyncio04.py
@@ -1,1 +1,17 @@
 # Starting task
+# Event Loop
+
+import asyncio
+
+async def greet():
+    print("Hello")
+    await asyncio.sleep(1)
+    print("World")
+
+asyncio.run(greet()) # + Create and run an event loop
+
+#
+# loop = asyncio.new_event_loop()
+# asyncio.set_event_loop(loop)
+# loop.run_until_complete(greet())
+# loop.close()
