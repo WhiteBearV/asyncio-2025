dbfc4c0bc1c01195bd0c45b0b567b2a6f675aa22
WhiteBearV <kusunzaza096@gmail.com>
2025-06-25 10:47:49 +0700

    push3

--- assignment04/asyncio03.py
+++ assignment04/asyncio03.py
@@ -1,2 +1,27 @@
 # Create 2 Tasks with High-Level API
 
+import asyncio
+
+async def download_image(name, delay):
+
+    print(f"{name} loading...")
+
+    await asyncio.sleep(delay)
+
+    print(f"{name} Loaded successfully!")
+
+async def main():
+
+# Create 2 tasks at once
+
+    task1 = asyncio.create_task(download_image("Image 1", 2))
+
+    task2 = asyncio.create_task(download_image("Image 2", 3))
+
+# Wait for both tasks to finish.
+
+    await task1
+
+    await task2
+
+asyncio.run(main())
