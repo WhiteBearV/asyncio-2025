34f636ed677e51c00a9f015742c69033843fc4d3
WhiteBearV <kusunzaza096@gmail.com>
2025-06-25 10:05:53 +0700

    รอบ1

--- assignment03/asyncio01.py
+++ assignment03/asyncio01.py
@@ -1,1 +1,9 @@
 # check the type of a coroutine
+import asyncio
+#define a corouine
+async def custom_coro():
+    #await another coroutine
+    await asyncio.sleep(1)
+
+coro = custom_coro()
+print(type(coro))
