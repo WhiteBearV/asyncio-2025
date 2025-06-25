bb792b8cec7f0f66135434af723077a10ae4c661
WhiteBearV <kusunzaza096@gmail.com>
2025-06-25 11:50:42 +0700

    push11

--- assignment04/asyncio11.py
+++ assignment04/asyncio11.py
@@ -1,1 +1,17 @@
 # task name
+
+import asyncio
+
+async def simple_task():
+
+    await asyncio.sleep(1)
+
+async def main():
+
+    task = asyncio.create_task(simple_task(), name="Load data")
+
+    print("Task name:", task.get_name())
+
+    await task
+
+asyncio.run(main())
