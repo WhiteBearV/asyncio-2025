eb7c3cb274a86559b951fd861a365d07e1506834
WhiteBearV <kusunzaza096@gmail.com>
2025-06-25 11:35:21 +0700

    push8

--- assignment04/asyncio08.py
+++ assignment04/asyncio08.py
@@ -1,1 +1,15 @@
 # get Exception
+import asyncio
+async def error_task():
+    await asyncio.sleep(1)
+    raise ValueError("Error!")
+
+async def main():
+    task = asyncio.create_task(error_task())
+    try:
+        await task
+    except Exception:
+        print("Exception is:", task.exception())
+
+
+asyncio.run(main())
