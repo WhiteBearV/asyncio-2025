b4a289c9e8ad7ca02df37d3efdd2b8ac8edaeba0
WhiteBearV <kusunzaza096@gmail.com>
2025-06-25 11:31:44 +0700

    push7

--- assignment04/asyncio07.py
+++ assignment04/asyncio07.py
@@ -1,1 +1,13 @@
 # get result
+import asyncio
+async def simple_task():
+    await asyncio.sleep(1)
+    return "completed!"
+
+async def main():
+    task = asyncio.create_task(simple_task())
+    await task
+    print("Reault Task:" ,task.result())
+
+
+asyncio.run(main())
