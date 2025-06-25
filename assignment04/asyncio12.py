6805082aa28f20c202226c47a8dc5a78e04262cc
WhiteBearV <kusunzaza096@gmail.com>
2025-06-25 11:51:37 +0700

    push12

--- assignment04/asyncio12.py
+++ assignment04/asyncio12.py
@@ -1,1 +1,15 @@
 # current task
+
+import asyncio
+
+async def show_current():
+
+    current = asyncio.current_task()
+
+    print("Current Task:", current.get_name())
+
+async def main():
+
+    await show_current()
+
+asyncio.run(main())
